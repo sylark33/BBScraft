c1f898e304c6cc36df2ca017b971a0013d8f8cdf
986698380 <986698380@qq.com>
2022-11-21 16:38:00 +0800

    123

---
+++ YiCraft-main/YiCraft/Controllers/reply_infosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YiCraft.Models;
+
+namespace YiCraft.Controllers
+{
+    public class reply_infosController : Controller
+    {
+        private YiCraftCoreEntities2 db = new YiCraftCoreEntities2();
+        // GET: reply_infos
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index([Bind(Include = "reply")] reply_infos reply_Infos)
+        {
+            if (ModelState.IsValid && reply_Infos.reply != null)
+            {
+                reply_Infos.theme_id = Convert.ToInt32(Session["did"]);
+                reply_Infos.ReplyAuthor = Session["uid"].ToString();
+                reply_Infos.ReplyTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+                db.reply_infos.Add(reply_Infos);
+                db.SaveChanges();
+                if (Session["uid"].ToString() == "FYZY" && reply_Infos.reply.Contains("已解决"))
+                {
+                    var pm = from u in db.discuss_infos
+                             where u.id ==reply_Infos.theme_id
+                             select u;
+                    discuss_infos di = new discuss_infos();
+                    foreach (var k in pm)
+                    {
+                        di = (discuss_infos)k;
+                    }
+                    di.solve = "已解决";
+                    db.Entry(di).State= EntityState.Modified;
+                    db.Entry(di).Property(u => u.id).IsModified = true;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index", "discuss_infos", "创建成功");
+            }
+                return RedirectToAction("Index", "discuss_infos", "创建失败");
+        }
+
+      
+    }
+}
---
+++ YiCraft-main/YiCraft/Controllers/yicraft_infosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using YiCraft.Models;
+
+namespace YiCraft.Controllers
+{
+    public class yicraft_infosController : Controller
+    {
+        private YiCraftCoreEntities2 db = new YiCraftCoreEntities2();
+
+        // GET: yicraft_infos
+        public ActionResult Index()
+        {
+            return View(db.yicraft_infos.ToList());
+        }
+
+        // GET: yicraft_infos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            yicraft_infos yicraft_infos = db.yicraft_infos.Find(id);
+            if (yicraft_infos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yicraft_infos);
+        }
+
+        // GET: yicraft_infos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: yicraft_infos/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,loginname,balance,status")] yicraft_infos yicraft_infos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.yicraft_infos.Add(yicraft_infos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(yicraft_infos);
+        }
+
+        // GET: yicraft_infos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            yicraft_infos yicraft_infos = db.yicraft_infos.Find(id);
+            if (yicraft_infos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yicraft_infos);
+        }
+
+        // POST: yicraft_infos/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,loginname,balance,status")] yicraft_infos yicraft_infos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(yicraft_infos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(yicraft_infos);
+        }
+
+        // GET: yicraft_infos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            yicraft_infos yicraft_infos = db.yicraft_infos.Find(id);
+            if (yicraft_infos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yicraft_infos);
+        }
+
+        // POST: yicraft_infos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            yicraft_infos yicraft_infos = db.yicraft_infos.Find(id);
+            db.yicraft_infos.Remove(yicraft_infos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
